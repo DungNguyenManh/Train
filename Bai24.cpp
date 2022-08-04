f5dcf2baee74b719d7dac5ae780c7e6bbdb01e00
your name <dung903698954@gmail.com>
2022-08-04 20:40:18 +0700

    add

---
+++ Bai24.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    printf("Uoc so cua %d la: ", n);
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    printf("\nUoc so le cua %d la: ", n);
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if (n % i == 0 && i % 2 != 0)
+        {
+            printf("%d ", i);
+        }
+    }
+
+    return 0;
+}
---
+++ Bai25.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+    printf("Nhap n: ");
+    scanf("%d", &n);
+    printf("Uoc so cua %d la: ", n);
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+            printf("%d ", i);
+    }
+
+    printf("\nUoc so chan cua %d la: ", n);
+    for (int i = 1; i <= n / 2; i++)
+    {
+        if (n % i == 0 && i % 2 == 0)
+        {
+            printf("%d ", i);
+            sum += i;
+        }
+    }
+    printf("\nTong cac uoc so chan cua %d la: %d", n, sum);
+
+    return 0;
+}
